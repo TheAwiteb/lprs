31afe4fd583f42efba89d368f43921ce0635f339
Awiteb <a@4rs.nl>
2024-05-10 17:58:59 +0300

    chore: Trim the added vault name

--- src/cli/add_command.rs
+++ src/cli/add_command.rs
@@ -50,6 +50,7 @@ pub struct Add {
 impl LprsCommand for Add {
     fn run(mut self, mut vault_manager: Vaults) -> LprsResult<()> {
         if !self.vault_info.is_empty() {
+            self.vault_info.name = self.vault_info.name.trim().to_string();
             self.vault_info.password = utils::user_password(self.password, "Vault password:")?;
             self.vault_info.custom_fields = self.custom_fields.into_iter().collect();
             vault_manager.add_vault(self.vault_info);
