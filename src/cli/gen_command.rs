907498b683a4aa619e5c1434dbae4bd102864d68
Awiteb <a@4rs.nl>
2024-05-20 08:00:20 +0300

    chore: Remove `www` from license url and remove whitespaces

--- src/cli/gen_command.rs
+++ src/cli/gen_command.rs
@@ -1,5 +1,5 @@
 // Lprs - A local CLI vaults manager. For human and machine use
-// Copyright (C) 2024  Awiteb <a@4rs.nl>
+// Copyright (C) 2024 Awiteb <a@4rs.nl>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -8,11 +8,11 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+// along with this program. If not, see <https://gnu.org/licenses/gpl-3.0.html>.
 
 use std::num::NonZeroU64;
 
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,5 @@
 // Lprs - A local CLI vaults manager. For human and machine use
-// Copyright (C) 2024  Awiteb <a@4rs.nl>
+// Copyright (C) 2024 Awiteb <a@4rs.nl>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -8,11 +8,11 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+// along with this program. If not, see <https://gnu.org/licenses/gpl-3.0.html>.
 
 use std::{fs, path::PathBuf};
 
@@ -79,7 +79,7 @@ impl_commands!(Commands, Add Remove List Clean Edit Gen Get Export Import Change
 
 /// Header message, used in the help message
 const HEADER: &str = r#"Copyright (C) 2024 Awiteb <a@4rs.nl>
-License GNU GPL-3.0-or-later <https://gnu.org/licenses/gpl.html>
+License GNU GPL-3.0-or-later <https://gnu.org/licenses/gpl-3.0.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 
